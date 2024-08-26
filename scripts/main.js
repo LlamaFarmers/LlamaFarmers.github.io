566477fb9fbf64645f0757cf0091ed10ae76f53b
LlamaFarmers <45830520+LlamaFarmers@users.noreply.github.com>
2024-08-26 01:08:05 -0500

    Expanded letter reroll
    Letter reroll button now resets the entire grid.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 document.getElementById("resetButton").addEventListener("click", function () {
-	 document.getElementById("testingSpace").innerHTML = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter11 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter12 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter13 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter21 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter22 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter23 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter31 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter32 = letterBank.charAt(Math.floor(Math.random()*26));
+	 letter33 = letterBank.charAt(Math.floor(Math.random()*26));
+	 document.getElementById("row1").innerHTML = letter11 + letter12 + letter13;
+	 document.getElementById("row2").innerHTML = letter21 + letter22 + letter23;
+	 document.getElementById("row3").innerHTML = letter31 + letter32 + letter33;
 });
 let myButton = document.querySelector("resetButton");
 let myTestingSpace = document.querySelector("testingSpace");
