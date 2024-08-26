45e2e909c70846e95043490287a5ea2c3d410579
LlamaFarmers <45830520+LlamaFarmers@users.noreply.github.com>
2024-08-26 00:52:35 -0500

    Fixed random letter gen
    Random letter generator was offset by 1. Occasionally returning a blank value and never returning "A"

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 document.getElementById("resetButton").addEventListener("click", function () {
-	 document.getElementById("testingSpace").innerHTML = letterBank.charAt(Math.floor(Math.random()*26) + 1);
+	 document.getElementById("testingSpace").innerHTML = letterBank.charAt(Math.floor(Math.random()*26));
 });
 let myButton = document.querySelector("resetButton");
 let myTestingSpace = document.querySelector("testingSpace");
