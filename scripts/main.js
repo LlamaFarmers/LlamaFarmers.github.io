9b7d1b91547f649daf141b6d93ddd66ee5337e63
LlamaFarmers <45830520+LlamaFarmers@users.noreply.github.com>
2024-08-26 00:19:04 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-document.querySelector("id:resetButton").addEventListener("click", function () {
+getElementById("resetButton").addEventListener("click", function () {
 	myTestingSpace = 'letterBank.charAt(Math.floor(Math.random()*26) + 1)';
 });
 let myButton = document.querySelector("resetButton")
