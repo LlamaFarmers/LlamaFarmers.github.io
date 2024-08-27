a277a59eb949695d9917dfb5742131dbd1e61473
LlamaFarmers <45830520+LlamaFarmers@users.noreply.github.com>
2024-08-27 15:33:53 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -211,7 +211,7 @@ document.getElementById("letter13button").addEventListener("click", function () 
 	});
 	
 document.getElementById("bluealien").addEventListener("click", function () {
-document.body.style.background = blue;
+document.body.style.background = "blue";
 });	
 	
 var gameScoreTemp
