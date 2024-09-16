af50c5c8fbeb742c4788c22ce8fe7fdbba03630c
LlamaFarmers <45830520+LlamaFarmers@users.noreply.github.com>
2024-09-16 15:14:25 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -562,4 +562,4 @@ var column1Barcode;
 var column2Barcode;
 var column3Barcode;
 var onOffState = "OFF";
-document.getElementById("versionnumber").innerHTML = "version: 83";
+document.getElementById("versionnumber").innerHTML = "version: 84";
