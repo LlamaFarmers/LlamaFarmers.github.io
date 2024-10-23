56a9052f7c223fc496ea37f0efd8159ce10ae2f5
LlamaFarmers <45830520+LlamaFarmers@users.noreply.github.com>
2024-10-23 15:16:10 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -365,6 +365,7 @@ function updateGridVisual() {
 	 column2Barcode = letter12.name + letter22.name + letter32.name;
 	 column3Barcode = letter13.name + letter23.name + letter33.name;
 	 if (possibleArtWords.length > 0) {
+		document.getElementById("arthinttext").style.color = blue;
 		document.getElementById("arthinttext").hidden = false;
 	 } else {
 		document.getElementById("arthinttext").hidden = true;
@@ -648,4 +649,4 @@ var column2Barcode;
 var column3Barcode;
 var onOffState = "OFF";
 document.body.style.backgroundColor = "silver"
-document.getElementById("versionnumber").innerHTML = "version: 91.2";
+document.getElementById("versionnumber").innerHTML = "version: 91.3.2";
